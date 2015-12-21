411d8dafb1971b0d81acc2398dce6f7d1a0253d3
Daniel Kronovet <kronovet@gmail.com>
2015-12-21 10:23:22 -0500

    Wrote multiplication program!

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @prod
+    M=0
+(LOOP)
+    @R0
+    D=M // load counter
+    @END
+    D;JEQ // leave loop if counter == 0
+    @prod
+    D=M // load product so far (psf)
+    @R1
+    D=D+M // psf + base
+    @prod
+    M=D // save psf
+    @R0
+    M=M-1 // decrement counter
+    @LOOP
+    0;JMP
+(END)
+    @prod
+    D=M // load product
+    @R2
+    M=D // put product in R2
--- test.sh
+++ test.sh
@@ -1,23 +1,37 @@
+assemble=false
+
 if [ $1 -le 3 ]
 then
-    cmd="tools/HardwareSimulator.sh"
-elif [ $1 -gt 3 & $1 -le 12 ]
+  cmd="tools/HardwareSimulator.sh"
+elif [ $1 -gt 3 ] && [ $1 -le 12 ]
 then
-    cmd="tools/CPUEmulator.sh"
+  cmd="tools/CPUEmulator.sh"
+  assemble=true
 else
-    cmd="tools/CPUEmulator.sh"
+  cmd="tools/CPUEmulator.sh"
 fi
 
 if [ $2 ]
 then
-    path=$1/$2
+  path=$1/$2
 else
-    path=$1
+  path=$1
+fi
+
+
+if [ $assemble ]
+then
+  for file in projects/$path/*.asm
+  do
+    echo "Assembling..."
+    echo $file
+    tools/Assembler.sh $file
+  done
 fi
 
 for file in projects/$path/*.tst
 do
-  echo
+  echo "Testing..."
   echo $file
   $cmd $file
 done
