23db0d08ee6dbd995897bb4c6f05b3118f7de52c
Daniel Kronovet <kronovet@gmail.com>
2015-12-21 09:37:56 -0500

    Generalized test.sh script

--- test.sh
+++ test.sh
@@ -1,6 +1,23 @@
-for file in projects/$1/*.tst
+if [ $1 -le 3 ]
+then
+    cmd="tools/HardwareSimulator.sh"
+elif [ $1 -gt 3 & $1 -le 12 ]
+then
+    cmd="tools/CPUEmulator.sh"
+else
+    cmd="tools/CPUEmulator.sh"
+fi
+
+if [ $2 ]
+then
+    path=$1/$2
+else
+    path=$1
+fi
+
+for file in projects/$path/*.tst
 do
-  echo ""
-  echo "$file"
-  tools/HardwareSimulator.sh "$file"
+  echo
+  echo $file
+  $cmd $file
 done
